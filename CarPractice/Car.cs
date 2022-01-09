6f61e5687e91e12069e06d97d7a1aa89fd1d5473
Lucy Ironmonger <37961811+lucy-ironmonger@users.noreply.github.com>
2022-01-09 21:30:15 +0000

    Add project files.

---
+++ CarPractice/Car.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarPractice
+{
+    public class Car
+    {
+        private string _colour = "red";
+
+        public string Colour
+        {
+            get 
+            { 
+                return _colour; 
+            }
+            set
+            {
+                _colour = value;
+            }
+        }
+    }
+}
---
+++ CarPractice/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace CarPractice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Car newCar = new Car();
+            Console.WriteLine($"The car colour is {newCar.Colour}");
+            Console.WriteLine($"What colour would you like to repaint it?");
+            var newColour = Console.ReadLine();
+            newCar.Colour = newColour;
+            Console.WriteLine($"You've resprayed the car {newCar.Colour} - what a badass!");
+        }
+    }
+}
